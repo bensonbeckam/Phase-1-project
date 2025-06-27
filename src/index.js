5bdc4d33f4c607a1e9c8880ebfba18a36a026288
Beckham <bensonbeckham98@gmail.com>
2025-06-27 11:41:08 +0300

    5th commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const cityInput =document.querySelector('.city-input')
 const searchBtn =document.querySelector('.search-btn')
 
+const weatherInfoSection =document.querySelector('.weather-info')
+const notFoundSection =document.querySelector('.not-found')
+const searchCitySection =document.querySelector('.search-city')
+
 const apiKey = '3679550d98adfc87181430b62457f5ac'
 
 searchBtn.addEventListener('click', () => {
@@ -20,14 +24,28 @@ cityInput.addEventListener('keydown', (event) =>{
 })
 
 async function getFetchData(endPoint, city) {
-    const apiUrl =`https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}`
+    const apiUrl =`https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`
 
     const response = await fetch(apiUrl)
 
     return response.json()
 }
+
 async function updateWeatherInfo(city) {
     const weatherData =await getFetchData('weather', city)
-    console.log(weatherData);
+
+    if (weatherData.cod != 200){
+        showDisplaySection(notFoundSection)
+        return
+    }
+    console.log(weatherData)
     
+    showDisplaySection(weatherInfoSection)
+}
+
+function showDisplaySection(section) {
+    [weatherInfoSection, searchCitySection, notFoundSection]
+    .forEach(section => section.style.display ='none')
+
+    section.style.display = 'flex'
 }
